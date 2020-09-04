068e5c5e80260e2d2b436658cad12fd8feb0e6c8
parvej1999 <1999monustp@gmail.com>
2020-09-05 01:46:14 +0530

    heroku debug

--- feedback/settings.py
+++ feedback/settings.py
@@ -49,13 +49,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
-MIDDLEWARE_CLASSES = (
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-)
 
 ROOT_URLCONF = "feedback.urls"
 
